bf8ec07e193042ba511e29e43eacfd7c328b0fc3
Shiva Shrestha <shivashrestha927@gmail.com>
2024-01-12 22:40:51 +0545

    Admin Panel Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
+import React from "react";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <MainPage />
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/fruit-store" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
